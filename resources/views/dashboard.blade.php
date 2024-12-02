c84252f53b5e45b786651adf38bfc8f0cffc346e
Marcus Moreira de Souza <marcus.moreira@gmail.com>
2024-12-02 08:06:28 -0300

    first commit

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,19 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="text-xl font-semibold leading-tight text-gray-800">
+            {{ __('Dashboard') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
+            <div class="overflow-hidden bg-white shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900">
+                    {{ __("You're logged in!") }}
+                    <x-bladewind.button has_spinner="true">Save User</x-bladewind.button>
+                    <x-bladewind.code />
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
